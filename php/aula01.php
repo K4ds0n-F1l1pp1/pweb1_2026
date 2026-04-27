890e0588bfcecfc23dc3c8dc12f1ebd72f4882cf
Kadson Filippi <kadsonfilippi2007@gmail.com>
2026-04-27 17:15:22 -0300

    aula 01
    Tentativa de algo, ameaçou fazer programa

--- php/aula01.php
+++ php/aula01.php
@@ -1,11 +1,82 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Verificador de Dados</title>
 </head>
 <body>
-    <? echo "Olá mundo (em PHP)!" ?>
+    
+    <h1>PREENCHA OS DADOS:</h1>
+
+    <form method="POST" action="">
+
+        <?php
+
+        $listName = 0;
+
+        while ($listName < 5)
+        {
+            echo "<br> <Label>Digite um nome: </br>
+                <input type='text' name='nome[]' maxlength='25'>";
+
+            $listName++;
+        }
+
+        ?>
+        
+        <br><br>
+
+        <Label>Digite sua Idade: </Label>
+        <input type="number" name="idade" maxlength="3">
+        <button type="submit">Inserir Dados</button>
+        <br>
+    </form>
+
+    <?php
+
+        $nomes = [];
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST")
+        {
+            $idade = $_POST["idade"];
+            $nome = $_POST["nome"];
+
+            if (!empty($idade) && !empty($nome))
+            {
+
+                if ($idade < 18)
+                {
+                    echo "<hr style='margin-top: 12px; margin-bottom: 12px'>Você é de menor! <br>";
+                }
+
+                if ($idade >= 18)
+                {
+                    echo "<hr style='margin-top: 12px; margin-bottom: 12px'>Você é de maior! <br>";
+                }
+
+                echo "Lista de Nomes:";
+                echo "<ul>";
+
+                    foreach ($nome as $itemNome)
+                    {
+                        if (!empty($itemNome))
+                        {
+                            echo "<li>" . htmlspecialchars($itemNome) . "</li>";
+                        }
+                    }
+
+                echo "</ul> <br>";
+            }
+
+        }
+
+    ?>
+
+    <a href="./aula01.php">
+        <br>
+        <button style="color: red;">RELOAD</button>
+    </a>
+
 </body>
 </html>
