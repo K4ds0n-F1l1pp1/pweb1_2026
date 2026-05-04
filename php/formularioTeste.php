434766016df1d7d5fcfd784f87cae18dcecf72e1
Kadson Filippi <kadsonfilippi2007@gmail.com>
2026-05-04 17:14:23 -0300

    Terminado

--- php/formularioTeste.php
+++ php/formularioTeste.php
@@ -3,15 +3,25 @@ include './header.php';
 ?>
 
 <div class="col">
-    <form action="resultadoFormAluno.php" method="get">
+    <form action="resultadoFormAluno.php" method="post">
         <div class="col-6">
             <label for="nome">Nome: </label>
-            <input type="text" name="nomeAluno" class="form-control" maxlength="35"> 
+            <input type="text" name="nomeAluno" class="form-control" maxlength="35" required> 
         </div>
 
         <div class="col-6">
-            <label for="nome">E-mail: </label>
-            <input type="email" name="emailAluno" class="form-control"> 
+            <label for="email">E-mail: </label>
+            <input type="email" name="emailAluno" class="form-control" required> 
+        </div>
+
+        <div class="col-6">
+            <label for="telefone">Telefone: </label>
+            <input type="text" name="telAluno" class="form-control" required> 
+        </div>
+
+        <div class="col-6">
+            <label for="senha">Senha: </label>
+            <input type="password" name="senhaAluno" class="form-control" required> 
         </div>
 
         <div class="col mt-4">
--- php/resultadoFormAluno.php
+++ php/resultadoFormAluno.php
@@ -4,8 +4,9 @@ include './header.php';
 
 <div class="col-6">
     <?php
-        echo "<p>" . $_GET['nomeAluno'] . "</p>";
-        echo "<p>" . $_GET['emailAluno'] . "</p>";
+        echo "<p>" . $_POST['nomeAluno'] . "</p>";
+        echo "<p>" . $_POST['emailAluno'] . "</p>";
+        echo "<p>" . $_POST['telefoneAluno'] . "</p>";
     ?>
 </div>
 
