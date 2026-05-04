41037ca600232f258ab0999f3a6bd43a759c03f1
Kadson Filippi <kadsonfilippi2007@gmail.com>
2026-05-04 16:09:26 -0300

    Arrumando detahes

--- php/aula01.php
+++ php/aula01.php
@@ -1,72 +1,69 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Revisão de PHP</title>
 </head>
 <body>
-    <?php echo "Oi mundo PHP <br>";
+    <?php 
+    
+    echo "Olá, mundo PHP!<br>";
 
-    $nome = "Felipe";
+    $nome = "Kadson";
     $idade = 18;
    
     echo "Nome: $nome, Idade: $idade.";
 
-
-    if ($idade >= 18){
+    if ($idade >= 18) {
         echo "<br> Maior de idade. <br>";
-    }else{
+    } else {
         echo "<br> Menor de idade. <br>";
     }
 
-
     $notas = [5, 6, 7, 8];
 
-
-    for ($i = 0; $i <count($notas); $i++){
-        echo $notas[$i]."<br>";
-    }
+    for ($i = 0; $i <count($notas); $i++)
+        {
+            echo $notas[$i]."<br>";
+        }
     echo "<br>";
-    foreach ($notas as $item) {
-        echo $item . "<br>";
-    }
-
+    foreach ($notas as $item)
+        {
+            echo $item . "<br>";
+        }
 
     $nomes = ["Felipe", "Dudu", "Kadinho"];
 
-
-    for ($i = 0; $i <count($nomes); $i++){
-        echo $nomes[$i]."<br>";
-    }
+    for ($i = 0; $i <count($nomes); $i++)
+        {
+            echo $nomes[$i]."<br>";
+        }
+        
         echo "<br>";
-    foreach ($nomes as $item) {
-        echo $item . "<br>";
-    }
-
+    foreach ($nomes as $item)
+        {
+            echo $item . "<br>";
+        }
 
     $carros = [
-        ["modelo"=>"Mustang", "cor" => "vermelho", "ano" => 1997],
-        ["modelo"=>"Sandeiro", "cor" => "Prata", "ano" => 2014],
-        ["modelo"=>"Brasilia", "cor" => "Amarela", "ano" => 1969],
+            ["modelo"=>"Mustang", "cor" => "vermelho", "ano" => 1997],
+            ["modelo"=>"Sauber C9", "cor" => "Silver", "ano" => 2014],
+            ["modelo"=>"Brasilia", "cor" => "Amarela", "ano" => 1969]
         ];
-
 
     echo $carros[0]["modelo"]. "-". $carros[0]["ano"];
 
-
     foreach($carros as $carro){
         echo "<br>";
-        foreach($carro as $item){
-            echo "Modelo:" . $item['modelo'] . "Ano:" . $item['ano'];
-        }
+        foreach($carro as $item)
+            {
+                echo "Modelo: " . $item['modelo'] . " - Ano: " . $item['ano'];
+            }
     }
-
 
     ?>
    
-
-
     <p>Meu Site <?= $carros[0]["modelo"]. "-". $carros[0]["ano"];?> ?></p>
 </body>
 </html>
